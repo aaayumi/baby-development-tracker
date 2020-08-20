d1727c6fb41f79540bad1192ab0df1b29abf8da7
Ayumi Saito <ayumisaito27@gmail.com>
2020-08-20 20:50:22 +0200

    add data

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,20 @@
 import React, { useState }from 'react';
 import { TodoListItem }from './TodoListItem'
+import { todos } from './Data'
 
-const todos: Todo[] = [ 
-{month: 1, text: ['bla','bla']},
-{month: 2, text: ['bla']}, 
-{month: 3, text: ['bla']},
-{month: 4, text: ['bla']},
-{month: 5, text: ['bla']},
-{month: 6, text: ['bla']},
-{month: 7, text: ['bla']},
-{month: 8, text: ['bla']},
-{month: 9, text: ['bla']},
-{month: 10, text: ['bla']},
-{month: 11, text: ['bla']},
-{month: 12, text: ['bla']},];
+// const todos: Todo[] = [ 
+// {month: 1, text: ['bla','bla']},
+// {month: 2, text: ['bla']}, 
+// {month: 3, text: ['bla']},
+// {month: 4, text: ['bla']},
+// {month: 5, text: ['bla']},
+// {month: 6, text: ['bla']},
+// {month: 7, text: ['bla']},
+// {month: 8, text: ['bla']},
+// {month: 9, text: ['bla']},
+// {month: 10, text: ['bla']},
+// {month: 11, text: ['bla']},
+// {month: 12, text: ['bla']},];
 
 const App: React.FC = () => {
 const [inputValue, setInputValue] = useState(0);
